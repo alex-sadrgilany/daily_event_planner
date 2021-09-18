d3614914d1e54964a8a8153b29c0ba4961596ae9
AS <asadrgilany7@gmail.com>
2021-09-17 22:21:14 -0400

    added timeCheck function to add and remove appropriate classes to the text areas based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,12 @@ console.log("It is currently " + currentDateTime);
 
 var saveText = function() {
 
+    // Grabbing the text from the sibling of the button the user just clicked
     var text = $(this).siblings(".description").val().trim();
+    // Grabbing the id of the parent
     var hour = $(this).parent().attr("id");
 
-    console.log(text);
-    console.log(hour);
-
+    // Adding an edge case for if the user accidentally saves a deleted field
     if (text==="") {
         var yesNo = confirm("Your event for this hour is empty. Are you sure you want to save?");
             if (yesNo) {
@@ -23,12 +23,13 @@ var saveText = function() {
                 loadText();
             }
     }
+    // Saving to Local Storage
     else {
         localStorage.setItem(hour, text);
     }
 }   
 
-function loadText () {
+var loadText = function() {
     // create an array of all the necessary hour ids
     var hourIdArray = ["am9", "am10", "am11", "pm12", "pm1", "pm2", "pm3", "pm4", "pm5"];
 
@@ -39,7 +40,33 @@ function loadText () {
     };
 };
 
+// Creating a function that will check the current hour against the hour of each row
+var checkTime = function() {
+    var currentHour = moment().hour();
+
+    // using split to capture only the number from each row's id
+    var plannerTime = $(".time-block").attr("id").split("m")[1];
+
+    if (plannerTime < currentHour) {
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+        $(this).addClass("past");
+    }
+    else if (plannerTime === currentHour) {
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    }
+    else if (plannerTime > currentHour) {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    }
+    
+}
 
 $(".saveBtn").on("click", saveText);
 
 loadText();
+
+$(".time-block").each(checkTime);
